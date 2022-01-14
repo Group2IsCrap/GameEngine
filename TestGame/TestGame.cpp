329c0a5042f7550c54e6d25287ac6bea1ecaa824
g012090i <fbob987@gmail.com>
2022-01-14 16:19:40 +0000

    Removed unessesary assert

--- TestGame/TestGame.cpp
+++ TestGame/TestGame.cpp
@@ -11,12 +11,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(nCmdShow);
 
-	Firelight::Maths::Vec3f vecTest = Firelight::Maths::Vec3f(0.0f, 1.0f, 1.0f);
-	vecTest.Normalise();
-
-	std::string vecString = std::to_string(vecTest.x) + ", " + std::to_string(vecTest.y) + ", " + std::to_string(vecTest.z);
-	ERROR_STANDARD(vecString)
-
 	if (Firelight::Engine::Instance().Initialise(hInstance, "Test Window", "windowClass", 1280, 720))
 	{
 		while (Firelight::Engine::Instance().ProcessMessages())
