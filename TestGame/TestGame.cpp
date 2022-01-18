030bc2a33b777ef651c8248b4a3f604dcdfc1af9
FergusGriggs <fbob987@gmail.com>
2022-01-18 18:40:36 +0000

    Minor changes to TestGame.cpp

--- TestGame/TestGame.cpp
+++ TestGame/TestGame.cpp
@@ -29,25 +29,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(nCmdShow);
 
-	if (Engine::Instance().Initialise(hInstance, "Test Window", "windowClass", Firelight::Maths::Vec2i(1280, 720)))
+	if (Engine::Instance().Initialise(hInstance, "Test Window", "windowClass", Maths::Vec2i(1280, 720)))
 	{
 		// ImGui Test code
 		ImGuiTestLayer* testLayer = new ImGuiTestLayer();
 		ImGuiManager::Instance()->AddRenderLayer(testLayer);
 		ImGuiManager::Instance()->RemoveRenderLayer(testLayer);
 
-		Graphics::Model* model = Firelight::Graphics::AssetManager::Instance().GetModel<Firelight::Graphics::FancyLitVertex>("cube.obj");
-		Graphics::Texture* textureTest = Firelight::Graphics::AssetManager::Instance().GetTexture("$ENGINE/Textures/transparency_test.jpg");
+		Graphics::Model* model = Graphics::AssetManager::Instance().GetModel<Graphics::FancyLitVertex>("cube.obj");
+		Graphics::Texture* textureTest = Graphics::AssetManager::Instance().GetTexture("$ENGINE/Textures/transparency_test.jpg");
 
 		while (Engine::Instance().ProcessMessages())
 		{
 			Engine::Instance().Update();
-
 			for (int pepeIndex = 0; pepeIndex < 100; ++pepeIndex)
 			{
 				Maths::Vec2f pepePos = Engine::Instance().GetWindowDimensionsFloat() * 0.5f + Maths::Vec2f(2.0f, 1.0f) * Maths::Vec2f::GetRandomVector() * Engine::Instance().GetWindowDimensionsFloat().y * 0.5f;
 				Graphics::GraphicsHandler::Instance().GetSpriteBatch()->PixelDraw(Maths::Rectf(pepePos.x - 100.0f, pepePos.y - 100.0f, 200.0f, 200.0f), textureTest);
 			}
+
+			Graphics::GraphicsHandler::Instance().GetSpriteBatch()->PixelDraw(Maths::Rectf(100.0f, 100.0f, 200.0f, 200.0f), Graphics::AssetManager::Instance().GetDefaultTexture(), 0);
 
 			Engine::Instance().RenderFrame();
 		}
