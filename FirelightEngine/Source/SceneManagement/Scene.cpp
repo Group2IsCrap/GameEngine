96e2119246d49758b5ce01e6bdaedfcb5b4b3095
Ben Nilsson <55544010+BenNilsson@users.noreply.github.com>
2022-01-18 15:15:17 +0000

    Project Hotfix

--- FirelightEngine/Source/SceneManagement/Scene.cpp
+++ FirelightEngine/Source/SceneManagement/Scene.cpp
@@ -2,39 +2,40 @@
 
 namespace Firelight::SceneManagement
 {
-	Scene::Scene()
+	Scene::Scene(const char* sceneName) : m_buildIndex(-1)
 	{
-
+		m_sceneName = sceneName;
 	}
 
 	Scene::~Scene()
 	{
+		for (auto entity : m_entities)
+		{
+			entity = nullptr;
+			delete entity;
+		}
 
+		m_entities.clear();
 	}
 
-	void Scene::CreateEntity(const std::string& name)
+
+	Firelight::ECS::Entity* Scene::CreateEntity()
 	{
-
+		return new Firelight::ECS::Entity();
 	}
 
-	void Scene::DestroyEntity(std::string entity)
+	void Scene::DestroyEntity(Firelight::ECS::Entity* entity)
 	{
 
 	}
 
 	void Scene::Update(double deltaTime)
 	{
-
+		UNREFERENCED_PARAMETER(deltaTime);
 	}
 
 	void Scene::GetMainCamera()
 	{
 
-	}
-
-	template<typename T>
-	void Scene::OnComponentAdded(std::string, T& component)
-	{
-		static_assert(false);
 	}
 }
--- FirelightEngine/Source/SceneManagement/Scene.h
+++ FirelightEngine/Source/SceneManagement/Scene.h
@@ -1,31 +1,56 @@
 #pragma once
 
-#include <string>
+#include "../ECS/Entity.h"
+
 #include <vector>
 
 namespace Firelight::SceneManagement
 {
-	// TODO : Forward declare entity class
-
-
 	class Scene
 	{
 	public:
-		std::vector<std::string*> m_entities; // TODO : Change string to Entity Wrapper class
+		std::vector<Firelight::ECS::Entity*> m_entities;
+		int m_buildIndex;
 
 	private:
-		template<typename T>
-		void OnComponentAdded(std::string, T& component); // TODO : Change string to Entity Wrapper
+		const char* m_sceneName;
 
 	public:
-		Scene();
+		Scene(const char* sceneName);
 		~Scene();
 
-		void CreateEntity(const std::string& name = std::string()); // TODO : Change void to Entity Wrapper class
-		void DestroyEntity(std::string entity); // TODO : Change string to Entity Wrapper class
+		/// <summary>
+		/// Returns the name of the scene
+		/// </summary>
+		const char* GetName() { return m_sceneName; }
 
+		/// <summary>
+		/// Returns the build index of the scene
+		/// </summary>
+		int GetBuildIndex() { return m_buildIndex; }
+
+		/// <summary>
+		/// Is responsible for creating a new entity and send it off to the global system using events.
+		/// This is the access point for any entity that has to exist in the current context
+		/// </summary>
+		/// <returns>The created entity</returns>
+		Firelight::ECS::Entity* CreateEntity();
+
+		/// <summary>
+		/// Destroys the specified entity
+		/// </summary>
+		/// <param name="entity"></param>
+		void DestroyEntity(Firelight::ECS::Entity* entity);
+
+		/// <summary>
+		/// Passes down the update loop to all entities in the current context
+		/// </summary>
+		/// <param name="deltaTime"></param>
 		void Update(double deltaTime);
 
+		/// <summary>
+		/// Returns the main camera in the scene
+		/// </summary>
 		void GetMainCamera(); // TODO : Change to Entity Wrapper class
 
 	};
