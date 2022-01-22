109f5be7d4c1e9ad30814ee6f4a2794017cf322e
Ben <benjaminnilsson1997@gmail.com>
2022-01-22 15:21:05 +0000

    [#70] Frame padding removed

--- FirelightEditor/Source/Panels/ViewportPanel.cpp
+++ FirelightEditor/Source/Panels/ViewportPanel.cpp
@@ -12,10 +12,12 @@ ViewportPanel::~ViewportPanel()
 
 void ViewportPanel::Draw()
 {
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
 	ImGui::Begin("Viewport");
 
 	ImVec2 windowSize = ImGui::GetContentRegionAvail();
 	ImGui::Image((void*)Firelight::Graphics::GraphicsHandler::Instance().GetFinalImage().m_shaderResourceView.Get(), { windowSize.x, windowSize.y });
 
 	ImGui::End();
+	ImGui::PopStyleVar();
 }
