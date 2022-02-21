6ace0f5c90162e912980bdc2fca86ae2b00caae6
Ben <benjaminnilsson1997@gmail.com>
2022-02-21 21:11:10 +0000

    [#151] Camera Following The Player

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -3,6 +3,9 @@
 #include "FModAudio.h"
 
 #include <Source/ECS/EntityWrappers/UICanvas.h>
+#include <Source/ECS/EntityWrappers/CameraEntity.h>
+#include <Source/Maths/Vec2.h>
+#include <Source/Graphics/AssetManager.h>
 
 #include "Source/Systems/PlayerSystem.h"
 #include "Source/Player/PlayerEntity.h"
@@ -24,16 +27,31 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 		// Register Systems
 		Firelight::Engine::Instance().GetSystemManager().RegisterGameSystem<PlayerSystem>();
 
+		// Camera
+		CameraEntity* camera = new CameraEntity();
+		Engine::Instance().SetActiveCamera(camera);
+
 		// Player
 		PlayerEntity* player = new PlayerEntity();
 
+		// Grass
+		SpriteEntity* test2 = new SpriteEntity();
+		test2->GetSpriteComponent()->texture = Graphics::AssetManager::Instance().GetTexture("Sprites/grassTexture.png");
+		test2->GetSpriteComponent()->pixelsPerUnit = 20.0f;
+		test2->GetSpriteComponent()->layer = 16;
+
 		// UI
-		UICanvas* canvas = new UICanvas();
+		UICanvas* canvas = new UICanvas(Firelight::Maths::Vec3f(1920, 1080, 0));
 		PlayerHealthUI* playerHealthUI = new PlayerHealthUI(canvas);
 
 		while (Firelight::Engine::Instance().ProcessMessages())
 		{
 			Engine::Instance().Update();
+
+			Maths::Vec3f desiredPosition = player->GetTransformComponent()->position;
+			Maths::Vec3f smoothPosition = Maths::Vec3f::Lerp(camera->GetTransformComponent()->position, desiredPosition, 5 * Firelight::Engine::Instance().GetTime().GetDeltaTime());
+			camera->GetTransformComponent()->position = smoothPosition;
+
 			snowFallAudio::FModAudio::AudioEngine::engine->Update();
 			Engine::Instance().RenderFrame();
 		}
--- Game/Source/UI/PlayerHealthUI.cpp
+++ Game/Source/UI/PlayerHealthUI.cpp
@@ -7,8 +7,11 @@
 
 PlayerHealthUI::PlayerHealthUI(Firelight::ECS::Entity* canvas)
 {
+	SetParent(canvas->GetEntityID());
 	Firelight::Events::EventDispatcher::AddListener<Firelight::Events::PlayerHealthChanged>(this);
 	GetSpriteComponent()->texture = Firelight::Graphics::AssetManager::Instance().GetTexture("Sprites/PanelTest.png");
+	GetWidgetComponent()->anchorSettings = Firelight::ECS::e_AnchorSettings::TopLeft;
+	SetDefaultDimensions(Firelight::Maths::Vec3f(400, 75, 0));
 }
 
 void PlayerHealthUI::HandleEvents(const char* event, void* data)
