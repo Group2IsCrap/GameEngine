7878f2c5f206c09b510ad8ca4eb7d092f7572edb
WhoGivesAFlux <danieljacksonportfolio@gmail.com>
2022-02-21 17:07:00 +0000

    Change Player System to playerEntity event

--- Game/Source/Systems/PlayerSystem.cpp
+++ Game/Source/Systems/PlayerSystem.cpp
@@ -12,39 +12,54 @@ static bool s_triggered = false;
 PlayerSystem::PlayerSystem()
 {
 	AddWhitelistComponent<PlayerComponent>();
+	playerEntity = nullptr;
+
+	m_playerEntityAddedCheckIndex = Firelight::Events::EventDispatcher::SubscribeFunction<Firelight::Events::ECS::OnEntityCreatedEvent>(std::bind(&PlayerSystem::CheckForPlayer, this));
+}
+
+PlayerSystem::~PlayerSystem()
+{
+	Firelight::Events::EventDispatcher::UnsubscribeFunction<Firelight::Events::ECS::OnEntityCreatedEvent>(m_playerEntityAddedCheckIndex);
+}
+
+void PlayerSystem::CheckForPlayer()
+{
+	if (playerEntity == nullptr && m_entities.size() > 0)
+	{
+		playerEntity = new PlayerEntity(m_entities[0]->GetEntityID());
+	}
 }
 
 void PlayerSystem::Update(const Firelight::Utils::Time& time)
 {
-	if (m_entities.size() <= 0 || m_entities.size() > 1)
+	if (playerEntity == nullptr)
 	{
 		return;
 	}
 
 	if (Firelight::Input::InputGet.KeyIsPress('W'))
 	{
-		m_entities[0]->GetComponent<Firelight::ECS::TransformComponent>()->position.y += static_cast<float>(Firelight::Engine::Instance().GetTime().GetDeltaTime() * m_entities[0]->GetComponent<PlayerComponent>()->speed);
+		playerEntity->GetTransformComponent()->position.y += static_cast<float>(Firelight::Engine::Instance().GetTime().GetDeltaTime() * playerEntity->GetComponent<PlayerComponent>()->speed);
 	}
 
 	if (Firelight::Input::InputGet.KeyIsPress('S'))
 	{
-		m_entities[0]->GetComponent<Firelight::ECS::TransformComponent>()->position.y -= static_cast<float>(Firelight::Engine::Instance().GetTime().GetDeltaTime() * m_entities[0]->GetComponent<PlayerComponent>()->speed);
+		playerEntity->GetTransformComponent()->position.y -= static_cast<float>(Firelight::Engine::Instance().GetTime().GetDeltaTime() * playerEntity->GetComponent<PlayerComponent>()->speed);
 	}
 
 	if (Firelight::Input::InputGet.KeyIsPress('A'))
 	{
-		m_entities[0]->GetComponent<Firelight::ECS::TransformComponent>()->position.x -= static_cast<float>(Firelight::Engine::Instance().GetTime().GetDeltaTime() * m_entities[0]->GetComponent<PlayerComponent>()->speed);
+		playerEntity->GetTransformComponent()->position.x -= static_cast<float>(Firelight::Engine::Instance().GetTime().GetDeltaTime() * playerEntity->GetComponent<PlayerComponent>()->speed);
 	}
 
 	if (Firelight::Input::InputGet.KeyIsPress('D'))
 	{
-		m_entities[0]->GetComponent<Firelight::ECS::TransformComponent>()->position.x += static_cast<float>(Firelight::Engine::Instance().GetTime().GetDeltaTime() * m_entities[0]->GetComponent<PlayerComponent>()->speed);
+		playerEntity->GetTransformComponent()->position.x += static_cast<float>(Firelight::Engine::Instance().GetTime().GetDeltaTime() * playerEntity->GetComponent<PlayerComponent>()->speed);
 	}
 
 	if (Firelight::Input::InputGet.KeyIsPress('K') && !s_triggered)
 	{
 		s_triggered = true;
-		playerEntity = new PlayerEntity(m_entities[0]->GetEntityID());
 		playerEntity->RemoveHealth(1);
 	}
 }
--- Game/Source/Systems/PlayerSystem.h
+++ Game/Source/Systems/PlayerSystem.h
@@ -8,8 +8,11 @@ class PlayerSystem : public Firelight::ECS::System
 {
 public:
 	PlayerSystem();
+	~PlayerSystem();
+	void CheckForPlayer();
 	void Update(const Firelight::Utils::Time& time) override;
 
 private:
+	size_t m_playerEntityAddedCheckIndex;
 	PlayerEntity* playerEntity;
 };
