8fcfa10d7d4a39c86409836b1e1c20c3e32ce647
Natalie Harvey <NatalieKHarvey@Hotmail.co.uk>
2022-05-13 01:46:15 +0100

    Island Shape Generating
    Needs work on:
    - North, South East, West spawning
    - outline shape with perlin noise
    - Tilemaps

--- Game/Source/PCG/BiomeGeneration.cpp
+++ Game/Source/PCG/BiomeGeneration.cpp
@@ -13,6 +13,7 @@ BiomeGeneration::BiomeGeneration()
 	: m_biomeNoise(nullptr)
 	, m_islandShapeNoise(nullptr)
 	, m_biome()
+	, m_radius(3)
 {
 	sm_biomeMap =
 	{
@@ -20,7 +21,8 @@ BiomeGeneration::BiomeGeneration()
 		Firelight::Graphics::AssetManager::Instance().GetTexture("$ENGINE/Textures/white.png"),
 		Firelight::Graphics::AssetManager::Instance().GetTexture("$ENGINE/Textures/yellow.png"),
 		Firelight::Graphics::AssetManager::Instance().GetTexture("$ENGINE/Textures/brown.png"),
-		Firelight::Graphics::AssetManager::Instance().GetTexture("$ENGINE/Textures/red.png")
+		Firelight::Graphics::AssetManager::Instance().GetTexture("$ENGINE/Textures/red.png"),
+		Firelight::Graphics::AssetManager::Instance().GetTexture("$ENGINE/Textures/tempbridgge.png")
 	};
 }
 
@@ -62,17 +64,18 @@ void BiomeGeneration::Draw()
 {
 	//TILE MAP DRAWS SO THIS FUNCTION SHOULDNT EXIST
 
+	DrawCircles();
 
-	Firelight::Maths::Rectf m_sourceRect = Firelight::Maths::Rectf(0.0f, 0.0f, -1.0f, -1.0f);
-	Firelight::Maths::Rectf m_destinationRect = Firelight::Maths::Rectf(-2.0f, -2.0f, 1.0f, 1.0f);
-	int m_layer = 32;
-	double m_rotation = 0.0;
+	//Firelight::Maths::Rectf m_sourceRect = Firelight::Maths::Rectf(0.0f, 0.0f, -1.0f, -1.0f);
+	//Firelight::Maths::Rectf m_destinationRect = Firelight::Maths::Rectf(-2.0f, -2.0f, 1.0f, 1.0f);
+	//int m_layer = 32;
+	//double m_rotation = 0.0;
 
-	for (size_t i = 0; i < 10; ++i)
-	{
-		Firelight::Maths::Rectf m_destinationRect = Firelight::Maths::Rectf(-5.0f + (i * 1.5), 3.0f, 1.0f, 1.0f);
-		Firelight::Graphics::GraphicsHandler::Instance().GetSpriteBatch()->WorldDraw(m_destinationRect, sm_biomeMap[RandomBiomeIndex(i)], m_layer, m_rotation, Firelight::Graphics::Colours::sc_white, m_sourceRect);
-	}
+	//for (size_t i = 0; i < 10; ++i)
+	//{
+	//	Firelight::Maths::Rectf m_destinationRect = Firelight::Maths::Rectf(-5.0f + (i * 1.5), 3.0f, 1.0f, 1.0f);
+	//	Firelight::Graphics::GraphicsHandler::Instance().GetSpriteBatch()->WorldDraw(m_destinationRect, sm_biomeMap[RandomBiomeIndex(i)], m_layer, m_rotation, Firelight::Graphics::Colours::sc_white, m_sourceRect);
+	//}
 }
 
 size_t BiomeGeneration::RandomBiomeIndex(unsigned int perlinIndex)
@@ -100,6 +103,63 @@ size_t BiomeGeneration::RandomBiomeIndex(unsigned int perlinIndex)
 	{
 		return 4;
 	}
+}
+
+void BiomeGeneration::DrawCircles()
+{
+	Firelight::Maths::Rectf m_initialSourceRect = Firelight::Maths::Rectf(0.0f, 0.0f, -1.0f, -1.0f);;
+	Firelight::Maths::Rectf m_destinationRect = Firelight::Maths::Rectf(0.0f, 3.0f, 1.0f, 1.0f);
+	Firelight::Maths::Rectf m_centre = Firelight::Maths::Rectf(-10.0f, 0.0f, 1.0f, 1.0f);
+	Firelight::Maths::Rectf m_curIslandCentre = m_centre;
+	int m_layer = 32;
+	double m_rotation = 0.0;
+
+	size_t numberOfIslands = 6;
+	size_t bridgeDimensionX = 3;
+	size_t bridgeDimensionY = 2;
+
+	m_radius = 4;
+
+	//for each island
+	Firelight::Maths::Rectf newDestRect = Firelight::Maths::Rectf(0.0f, 0.0f, 1.0f, 1.0f);
+	for (size_t iterator = 0; iterator < numberOfIslands; ++iterator)
+	{
+		for (int x = -m_radius; x <= m_radius; ++x)
+		{
+			for (int y = -m_radius; y <= m_radius; ++y)
+			{
+				// if distance is within radius
+				newDestRect = Firelight::Maths::Rectf(m_curIslandCentre.x + x, m_curIslandCentre.y + y, 1.0f, 1.0f);
+				if (CalculateDistance(newDestRect, m_curIslandCentre) <= m_radius)
+				{
+					Firelight::Graphics::GraphicsHandler::Instance().GetSpriteBatch()->WorldDraw(newDestRect, sm_biomeMap[RandomBiomeIndex(iterator)], m_layer, m_rotation, Firelight::Graphics::Colours::sc_white, m_initialSourceRect);
+				}
+			}
+		}
+
+		//draw bridge
+		newDestRect.x = m_curIslandCentre.x + m_radius + 1;
+		newDestRect.y = m_curIslandCentre.y;
+		for (size_t x = 0; x < bridgeDimensionX; ++x)
+		{
+			for (size_t y = 0; y < bridgeDimensionY; ++y)
+			{
+				Firelight::Graphics::GraphicsHandler::Instance().GetSpriteBatch()->WorldDraw(newDestRect, sm_biomeMap[5], m_layer, m_rotation, Firelight::Graphics::Colours::sc_white, m_initialSourceRect);
+				newDestRect.y = newDestRect.y + 1;
+			}
+			newDestRect.x = newDestRect.x + 1;
+			newDestRect.y = m_curIslandCentre.y;
+		}
+
+		// new island start
+		m_curIslandCentre.x = m_curIslandCentre.x + (m_radius * 2) + bridgeDimensionX + 1;
+	}
+
+}
+
+size_t BiomeGeneration::CalculateDistance(Firelight::Maths::Rectf rect1, Firelight::Maths::Rectf rect2)
+{
+	return sqrt(pow(rect1.x - rect2.x, 2) + pow(rect1.y - rect2.y, 2));
 }
 
 size_t BiomeGeneration::CalculateIslandShape(int perlinIndex)
--- Game/Source/PCG/BiomeGeneration.h
+++ Game/Source/PCG/BiomeGeneration.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Noise.h"
 #include "Biomes.h"
+#include "..\FirelightEngine\Source\Maths\Rect.h"
 #include <map>
 
 class BiomeGeneration
@@ -15,6 +16,10 @@ public:
 	size_t RandomBiomeIndex(unsigned int perlinIndex);
 	size_t CalculateIslandShape(int perlinIndex);
 
+	void DrawCircles();
+
+	size_t CalculateDistance(Firelight::Maths::Rectf rect1, Firelight::Maths::Rectf rect2);
+
 	void Draw();
 	void Render();
 
@@ -28,4 +33,6 @@ private:
 
 	std::vector< Firelight::Graphics::Texture*> sm_biomeMap;
 	Biome m_biome;
+
+	int m_radius;
 };
