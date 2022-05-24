88eded86ef104ae6fdff90a99cede9e69efc721c
WhoGivesAFlux <danieljacksonportfolio@gmail.com>
2022-05-24 16:45:48 +0100

    TitleScreen

--- Game/Source/Inventory/Inventory.cpp
+++ Game/Source/Inventory/Inventory.cpp
@@ -376,7 +376,9 @@ bool Inventory::AddItem(Firelight::ECS::EntityID item)
 						text->hidden = true;
 						text->text.SetTextHeight(30);
 						text->layer = 100000;
+						text->text.SetColour(Firelight::Graphics::Colours::sc_black);
 						text->text.SetTextAnchor(Firelight::Graphics::TextAnchor::e_BotRight);
+						text->text.SetAnchorPosition(-1.0f, -1.0f);
 
 						if (inventoryData->isDisplay) {
 							icon->SetParent(slot->slotID);
