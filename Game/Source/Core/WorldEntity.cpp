8a8f50c62582486812b070ecd4baf568496cd929
Natalie Harvey <NatalieKHarvey@Hotmail.co.uk>
2022-05-25 20:46:26 +0100

    Revert "Revert "Random Seed Everytime. Forest biome will always be starting biome""
    This reverts commit 37c2a6281ff77426b0cc571fbfed5dd2c577b41f.

--- Game/Source/Core/WorldEntity.cpp
+++ Game/Source/Core/WorldEntity.cpp
@@ -3,7 +3,7 @@
 
 WorldEntity::WorldEntity() : Firelight::ECS::SpriteEntity()
 {
-	GetSpriteComponent()->texture = Firelight::Graphics::AssetManager::Instance().GetTexture("Sprites/grassTexture.png");
+	GetSpriteComponent()->texture = Firelight::Graphics::AssetManager::Instance().GetTexture("Sprites/Terrain/water.png");
 	GetSpriteComponent()->pixelsPerUnit = 20.0f;
 	GetSpriteComponent()->layer = static_cast<int>(RenderLayer::World);
 	this->GetIDComponent()->name = "World Entity";
